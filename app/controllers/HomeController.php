c2ddfe68aa2a2bac5ee103d106f10ec1d9175a67
guguyb1 <guguyb111@gmail.com>
2023-12-28 15:40:25 +0700

    Add HomeController and update routes

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+// File: controllers/home.php
+
+class HomeController
+{
+    public function index()
+    {
+        echo 'Trang chủ';
+    }
+
+    public function maGiamGia()
+    {
+        echo 'Mã giảm giá';
+    }
+}
+
+
+?>
--- index.php
+++ index.php
@@ -2,11 +2,44 @@
 require 'vendor/autoload.php';
 session_start();
 $router = new \Bramus\Router\Router();
+
 $router->get('/', function () {
-    echo 'Hello, world!';
+    echo 'Trang chủ';
 });
+
+$router->get('/trang-chu', function () {
+    require 'app/controllers/HomeController.php';
+    $HomeController = new HomeController();
+    $HomeController->index();
+});
+
 $router->get('/ma-giam-gia', function () {
-    echo 'mã giảm giá';
+    echo 'Mã giảm giá';
 });
+
+$router->get('/theo-doi-ma-san-pham', function () {
+    echo 'Theo dõi mã sản phẩm';
+});
+
+$router->get('/tin-khuyen-mai', function () {
+    echo 'Tin khuyến mại';
+});
+
+$router->get('/huong-dan', function () {
+    echo 'Hướng dẫn';
+});
+
+$router->get('/ma-giam-gia/nha-cung-cap', function () {
+    echo 'Mã giảm giá/ Nhà cung cấp';
+});
+
+$router->get('/tin-khuyen-mai/chi-tiet', function () {
+    echo 'Tin khuyến mại/ Chi tiết tin khuyến mại';
+});
+
+$router->get('/huong-dan/chi-tiet', function () {
+    echo 'Hướng dẫn/ Chi tiết hướng dẫn';
+});
+
 $router->run();
 ?>
