c985cd64b1a311b0454e9f239c92238269255ec2
guguyb1 <guguyb111@gmail.com>
2023-12-29 14:56:14 +0700

    Add new images and update home.js and promotion.css files

---
+++ app/views/home/sectionProvider.php
@@ -0,0 +1,63 @@
+<!-- Hợp tác -->
+<section id="testimonials" class="testimonials pt-4">
+    <div class="container" data-aos="fade-up">
+        <hr class="custom-hr pt-4" />
+        <div class="section-title">
+            <h2>NHÀ CUNG CẤP NỔI BẬT</h2>
+        </div>
+        <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="200">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="testimonial-item">
+                        <div class="profile mt-auto">
+                            <a href="#" target="_blank">
+                                <img src="public\images\logo\ladaza.png" class="testimonial-img" alt="" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div class="testimonial-item">
+                        <div class="profile mt-auto">
+                            <a href="https://www.misa.vn/cong-ty/" target="_blank">
+                                <img src="public\images\logo\shopee.png" class="testimonial-img" alt="" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div class="testimonial-item">
+                        <div class="profile mt-auto">
+                            <a href="#" target="_blank">
+                                <img src="public\images\logo\shopeefood.png" class="testimonial-img" alt="" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div class="testimonial-item">
+                        <div class="profile mt-auto">
+                            <a href="#" target="_blank">
+                                <img src="public\images\logo\tiki.png" class="testimonial-img" alt="" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div class="testimonial-item">
+                        <div class="profile mt-auto">
+                            <a href="#" target="_blank">
+                                <img src="public\images\logo\tiktok.png" class="testimonial-img" alt="" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+</section>
---
+++ public/js/home.js
@@ -0,0 +1,25 @@
+new Swiper(".testimonials-slider", {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      },
+  
+      1200: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  
