7784d6f2ad171e505f0bbf0e5d9637a60f69c6ca
guguyb1 <guguyb111@gmail.com>
2024-01-06 18:27:30 +0700

    Add new route for testing and scraping data from a website

--- index.php
+++ index.php
@@ -3,12 +3,37 @@ require 'vendor/autoload.php';
 session_start();
 $router = new \Bramus\Router\Router();
 
+
+
+
 $router->get('/', function () {
     require 'app/controllers/HomeController.php';
     $HomeController = new HomeController();
     $HomeController->index();
-   
+
 });
+
+$router->get('/test', function () {
+    date_default_timezone_set('Asia/Bangkok'); // Set the time zone to UTC+7 (Asia/Bangkok)
+    $date = date('Y-m-d H:i:s');
+    echo $date;
+    echo "<br>";
+    include_once('simple_html_dom.php');
+    $dom = file_get_html('https://shopee.vn/Balo-%C4%90i-H%E1%BB%8Dc-%C4%90i-Ch%C6%A1i-Nam-N%E1%BB%AF-Th%E1%BB%9Di-Trang-H%C3%A0n-Qu%E1%BB%91c-Nhi%E1%BB%81u-Ng%C4%83n-Ti%E1%BB%87n-D%E1%BB%A5ng-%C4%91%E1%BB%B1ng-v%E1%BB%ABa-laptop-15.6inch-KT105-i.1144976040.24305621990');
+    $string_id = $dom->find('.ha5ReG a', 0)->href;
+    echo "id:" . explode("/", $string_id)[4];
+    echo "<br>";
+    echo "tên sản phẩm:" . $dom->find('._5f9gl5 span', 0)->plaintext;
+    echo "<br>";
+    echo "giá:" . $dom->find('.TVzooJ', 0)->plaintext;
+    echo "<br>";
+    echo "vote rate:" . $dom->find('.sbAxkj', 0)->plaintext;
+    echo "<br>";
+    echo "Lượt bán:" . $dom->find('.product-review__sold-count', 0)->plaintext;
+    echo "<br>";
+
+});
+
 
 $router->get('/trang-chu', function () {
     require 'app/controllers/HomeController.php';
